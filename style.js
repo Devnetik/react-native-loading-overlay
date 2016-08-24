30a5e4b5e7ff1d3a2f12297837f75120b1986fda
Michael Malura <maluramichael@googlemail.com>
2016-08-24 11:59:51 +0200

    Initialize component

---
+++ style.js
@@ -0,0 +1,23 @@
+/**
+ * Created by michaelmalura on 30/07/16.
+ */
+
+import React, {Component} from 'react';
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+	background: {
+		position       : 'absolute',
+		top            : 0,
+		left           : 0,
+		bottom         : 0,
+		right          : 0,
+		flex           : 1,
+		justifyContent : 'center',
+		alignItems     : 'center'
+	},
+	text      : {
+		color    : 'white',
+		marginTop: 20
+	}
+});
